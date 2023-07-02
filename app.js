c291c7785703c98e85e8e6b2505d62a41d7897b6
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-07-02 22:01:34 +0100

    change methods of Ship factory to track hits

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict'
 
 const Ship = (length) => {
-    let timesHit = 0
+    let timesHit = []
     const hit = () => {
-        timesHit++
+        timesHit.push(1) // using a number instead of array returns 0, issue with scope/closure 
         console.log(timesHit)
     }
     let isSunk = false
-    return { length, hit, isSunk }
+    return { length, timesHit, hit, isSunk }
 }
 
-const newShip = Ship(2)
+let newShip = Ship(2)
 console.log(newShip)
 
 module.exports = add, newShip
