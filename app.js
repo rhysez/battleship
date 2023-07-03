980f3c6ad9ebf90f77731edbebd3b38e5c9e563b
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-07-03 11:21:34 +0100

    implement early iteration of Gameboard factory

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const Ship = (size) => {
+const Ship = (size, cord1, cord2) => {
     let timesHit = 0
     let sunkStatus = false
     function hit(){
@@ -8,13 +8,30 @@ const Ship = (size) => {
         console.log(this.timesHit)
     }
     function isSunk(){
-        this.timesHit == this.size * 2 ? console.log('Ship sunk') : console.log('Ship not sunk')
         this.timesHit == this.size * 2 ? this.sunkStatus = true : this.sunkStatus = false
     }
-    return { size, timesHit, sunkStatus, hit, isSunk }
+    return { size, cord1, cord2, timesHit, sunkStatus, position, hit, isSunk }
 }
 
-let newShip = Ship(2)
+const Gameboard = () => {
+    function placeShip(){
+        let newShip = Ship(2, 5, 7)
+        return newShip
+    }
+    function recieveAttack(attack){
+        // takes co-ords, if attack hits a ship then send 'hit' function to correct ship
+        // not sure if this works yet
+        attack >= newShip.cord1 && attack <= newShip.cord2 ? newShip.hit() : this.miss()
+    } 
+
+    let missedAttacks = []
+
+    function miss(){
+        alert('You missed!')
+        missedAttacks.push('miss')
+    }
+
+    return { placeShip, recieveAttack, missedAttacks, miss }
+}
 
 module.exports = newShip
-
--- app.test.js
+++ app.test.js
@@ -4,3 +4,15 @@ const newShip = require('./app')
 test('checks timesHit starts at 0', () => {
     expect(newShip.timesHit).toStrictEqual(0)
 }) 
+// pass
+test('checks sunkStatus is falsey', () => {
+    expect(newShip.sunkStatus).toBe(false)
+})
+// pass
+test('checks size is correct', () => {
+    expect(newShip.size).toBe(2)
+})
+// pass
+test('checks isSunk returns correct calculation', () => {
+    expect(newShip.isSunk()).toBeFalsy()
+})
