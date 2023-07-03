4d190e69e45aa5fc9f477e7558541c56578bf42a
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-07-03 10:19:35 +0100

    add new ternary to isSunk() to change sunkStatus to true or false based on whether ship has been sunk

--- app.js
+++ app.js
@@ -2,14 +2,16 @@
 
 const Ship = (size) => {
     let timesHit = 0
+    let sunkStatus = false
     function hit(){
         this.timesHit += 1
         console.log(this.timesHit)
     }
     function isSunk(){
         this.timesHit == this.size * 2 ? console.log('Ship sunk') : console.log('Ship not sunk')
+        this.timesHit == this.size * 2 ? this.sunkStatus = true : this.sunkStatus = false
     }
-    return { size, timesHit, hit, isSunk }
+    return { size, timesHit, sunkStatus, hit, isSunk }
 }
 
 let newShip = Ship(2)
